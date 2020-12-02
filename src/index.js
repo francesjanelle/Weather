528dc4b507d5e9fe4e89a49536aa999a5127c63d
Frances-Janelle Torres <frances.janelle.torres@gmail.com>
2020-12-02 10:35:04 -0700

    Update index.js
    working on date

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 // alert("Hello world!");
+let currentDay = new Date();
+
+let currentDate = currentDay.getDate();
+
+let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+]
+
+let today = days[currentDay.getDay()];
+
+
+const apiKey = "deb4d0036edfa966c7a36750fd024ceb"
+let city = "Denver"
+const metric = "metric"
+const fahrenheit = "fahrenheit"
