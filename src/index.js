264cc9c50b1715509d6cc29f45d90a3d2bc21ef2
Frances-Janelle Torres <frances.janelle.torres@gmail.com>
2020-12-02 11:31:29 -0700

    Update index.js
    code not working. Will research why.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-// alert("Hello world!");
+// Date Beginning
+
 let currentDay = new Date();
 
 let currentDate = currentDay.getDate();
@@ -16,7 +17,6 @@ let days = [
 let today = days[currentDay.getDay()];
 
 let year = currentDay.getFullYear();
-//alert(year);
 
 let months = [
     "Jan",
@@ -34,13 +34,27 @@ let months = [
 ]
 
 let month = months[currentDay.getMonth()];
-//alert(month)
-//alert(`Today is ${today}. ${month} ${currentDate}, ${year}`)
 
 let now = document.querySelector(".now")
-    now.innerHTML = `${month} ${currentDate}, ${year}`
+    now.innerHTML = `${today}, ${month} ${currentDate}, ${year}`
 
-const apiKey = "deb4d0036edfa966c7a36750fd024ceb"
-let city = "Denver"
-const metric = "metric"
-const fahrenheit = "fahrenheit"
+// API Beginning
+
+function displayWeather(response) { 
+    let h3City = document.querySelector(".city").value;
+// document.querySelector(".city").innerHTML = response.data.name;
+    document.querySelector(".nowTemp").innerHTML = response.data.main.temp;
+}
+
+function searchBox(event)
+{
+    event.preventDefault();
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${metric}`
+    const apiKey = "deb4d0036edfa966c7a36750fd024ceb"
+    let city = document.querySelector("#searchLocation").value
+    const metric = "metric"
+
+    axios.get(apiUrl).then(displayWeather)
+    
+}   
+// const fahrenheit = "fahrenheit"
