a74f620bced48b331a3df13bb939b0d07ff35037
Frances-Janelle Torres <frances.janelle.torres@gmail.com>
2020-12-03 11:00:52 -0700

    Updated Code
    got axios and innerhtml working

--- src/index.js
+++ src/index.js
@@ -40,11 +40,24 @@ let now = document.querySelector(".now")
 
 ///
 
+function showTemp(response) { 
+    console.log(response.data);
+    let temperature = document.querySelector("#nowTemp")
+    let searchCity = document.querySelector("#city")
+    let description = document.querySelector("#description")
+    temperature.innerHTML = Math.round(response.data.main.temp);
+    searchCity.innerHTML = response.data.name;
+    description.innerHTML = response.data.weather[0].description; 
+}
+
 // const apiKey = "deb4d0036edfa966c7a36750fd024ceb";
-let city = "Denver"
+let  city = "Denver"
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=deb4d0036edfa966c7a36750fd024ceb&units=metric`
 
-console.log(apiUrl)
+axios.get(apiUrl).then(showTemp);
+
+
+/// 
 
 function nowCelsius(event) {
     event.preventDefault();
